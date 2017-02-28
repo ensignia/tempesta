c0288d4ad4cdf41b4d6f05aea412507bff4d29a4
ryanwr <ryantwr@gmail.com>
2017-02-28 11:49:22 +0000

    Added size to icon

--- src/app/components/Icon/Icon.js
+++ src/app/components/Icon/Icon.js
@@ -1,15 +1,18 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import cx from 'classnames';
 import s from './Icon.css';
 
 class Icon extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
+    size: PropTypes.num,
   };
 
   render() {
-    const { name } = this.props;
-    return <i className={s['material-icons']}>{name}</i>;
+    const { name, size } = this.props;
+    const sizeClass = [18, 24, 36, 48].includes(size) ? `md-${size}` : '';
+    return <i className={cx(s['material-icons'], sizeClass)}>{name}</i>;
   }
 }
 
